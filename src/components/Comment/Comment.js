4641db949a95e4a88805de40b0c1d929512d5b22
bkyoung311 <bkyoung311@gmail.com>
2017-04-19 01:28:08 +0900

    Ajax-tut first

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Comment.css';
+
+const Comment = ({name, body}) => {
+    return (
+        <li className="Comment">
+            <p>
+                <b>111</b>1111
+            </p>
+        </li>
+    );
+};
+
+export default Comment;
---
+++ src/containers/PostContainer/PostContainer.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import { PostWrapper, Navigate, Post } from '../../components';
+import * as service from '../../services/post';
+
+class PostContainer extends Component {
+
+    constructor(props) {
+        super();
+
+        this.state = {
+            postId: 1,
+            fetching: false,
+            post: {
+                title: null,
+                body: null
+            },
+            comments: []
+        };
+    }
+
+    componentDidMount() {
+        this.fetchPostInfo(1);
+    }
+
+    fetchPostInfo = async (postId) => {
+
+        this.setState({
+            fetching: true
+        })
+
+        const info = await Promise.all([
+            service.getPost(postId),
+            service.getComments(postId)
+        ]);
+
+        const {title, body} = info[0].data;
+
+        const comments = info[1].data;
+
+        this.setState({
+            postId,
+            post: {
+                title,
+                body
+            },
+            comments,
+            fetching: false
+        });
+    }
+
+    handleNavigateClick = (type) => {
+        const postId = this.state.postId;
+
+        if(type === 'NEXT') {
+            this.fetchPostInfo(postId+1);
+        } else {
+            this.fetchPostInfo(postId-1);
+        }
+    }
+
+    render() {
+        const {postId, fetching, post, comments} = this.state;
+
+        return(
+            <PostWrapper>
+                <Navigate
+                    postId={postId}
+                    disabled={fetching}
+                    onClcik={this.handleNavigateClick}
+                />
+                <Post
+                    title={post.title}
+                    body={post.body}
+                    comments={comments}
+                />
+            </PostWrapper>
+        );
+    }
+}
+
+export default PostContainer;
